522f584f8dbde4a99090ca16d42488d58fb4846e
Let Marçal <leticiamarcalmatias@gmail.com>
2021-09-23 20:31:16 -0300

    Criando a classe pessoa

---
+++ src/br/com/zup/ModelagemDeClasses/Pessoa.java
@@ -0,0 +1,50 @@
+package br.com.zup.ModelagemDeClasses;
+
+/*
+Crie uma Classe Pessoa, contendo os atributos encapsulados, com seus respectivos seletores (getters) e modificadores
+(setters). Atributos: String nome; String endereco; String telefone;
+ */
+public class Pessoa {
+    //Atributos
+    private String nome;
+    private String endereco;
+    private String telefone;
+
+    //Método construtor
+    public Pessoa(){
+
+    }
+
+    public Pessoa(String nome, String endereco, String telefone) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+    //Getters and Setters
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
