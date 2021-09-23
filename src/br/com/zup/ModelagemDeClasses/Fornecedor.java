076529fd73101696caa2b605fc62767a1f729f44
Let Marçal <leticiamarcalmatias@gmail.com>
2021-09-23 20:38:40 -0300

    Incluindo a classe Fornecedor

--- src/br/com/zup/ModelagemDeClasses/Fornecedor.java
+++ src/br/com/zup/ModelagemDeClasses/Fornecedor.java
@@ -1,2 +1,37 @@
-package br.com.zup.ModelagemDeClasses;public class Fornecedor {
+package br.com.zup.ModelagemDeClasses;
+
+/*
+Considere, como subclasse da classe Pessoa (desenvolvida no item anterior) a classe Fornecedor. Considere que cada
+instância da classe Fornecedor tem, para além dos atributos que caracterizam a classe Pessoa, os atributos valorCredito
+(correspondente ao crédito máximo atribuído ao fornecedor) e valorDivida (montante da dívida para com o fornecedor).
+
+ */
+public class Fornecedor extends Pessoa{
+    //Atributos distintos da classe Fornecedor
+    private double valorCredito;
+    private double valorDivida;
+
+    //Método construtor
+    public Fornecedor(String nome, String endereco, String telefone, double valorCredito, double valorDivida) {
+        super(nome, endereco, telefone);
+        this.valorCredito = valorCredito;
+        this.valorDivida = valorDivida;
+    }
+
+    //Getters and Setters
+    public double getValorCredito() {
+        return valorCredito;
+    }
+
+    public void setValorCredito(double valorCredito) {
+        this.valorCredito = valorCredito;
+    }
+
+    public double getValorDivida() {
+        return valorDivida;
+    }
+
+    public void setValorDivida(double valorDivida) {
+        this.valorDivida = valorDivida;
+    }
 }
