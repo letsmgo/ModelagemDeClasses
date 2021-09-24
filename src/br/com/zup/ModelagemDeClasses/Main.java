105b85384098c3d3b2d44a1712c62920f49110b4
Let Marçal <leticiamarcalmatias@gmail.com>
2021-09-23 21:53:57 -0300

    Testando funcionalidades da classe Administrador e o método calcular salário com @Override

--- src/br/com/zup/ModelagemDeClasses/Main.java
+++ src/br/com/zup/ModelagemDeClasses/Main.java
@@ -6,6 +6,8 @@ public class Main {
         Fornecedor fornecedor1 = new Fornecedor("Valecard", "Avenida Jacarandá 200", "3432323232", 10000, 5000);
         //Instanciando o colaborador
         Colaborador colaborador1 = new Colaborador("Let Marçal", "Genarino Cazabona 309", "349999999", 12, 12000, 1550);
+        //Instanciando o administrador
+        Administrador adm1 = new Administrador("André","Logo ali 300","348888888",11,50000,5000,3000);
 
         //Testando funcionalidades da classe Fornecedor
         System.out.println("NOME DO FORNECEDOR: " + fornecedor1.getNome());
@@ -25,7 +27,19 @@ public class Main {
         System.out.println("VALOR IMPOSTO DO COLABORADOR: R$" + colaborador1.getImposto() + "\n");
 
         //Testando o método calcular salário
-        System.out.println("SALÁRIO LÍQUIDO DO COLABORADOR É: R$" + colaborador1.calcularSalario());
+        System.out.println("SALÁRIO LÍQUIDO DO COLABORADOR É: R$" + colaborador1.calcularSalario() + "\n");
+
+        //Testando funcionalidades da classe Administrador
+        System.out.println("NOME DO ADMINISTRADOR: " + adm1.getNome());
+        System.out.println("TELEFONE DO ADM: " + adm1.getTelefone());
+        System.out.println("CODIGO DO SETOR DO ADM: " + adm1.getCodigoSetor());
+        System.out.println("SALÁRIO DO ADM: R$" + adm1.getSalarioBase());
+        System.out.println("VALOR DO IMPOSTO DO ADM: R$" + adm1.getImposto());
+        System.out.println("VALOR DA AJUDA DE CUSTO DO ADM: R$" + adm1.getAjudaDeCusto());
+
+        //Testando funcionalidade do método calcular salário para ADM
+        System.out.println("SALARIO LIQUIDO DO ADM: R$" + adm1.calcularSalario() + "\n");
+
 
     }
 }
