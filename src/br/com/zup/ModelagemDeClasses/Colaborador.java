ed116d09f3e97613f2ed3302847e8bba40d53c97
Let Marçal <leticiamarcalmatias@gmail.com>
2021-09-23 21:11:16 -0300

    Implementando a classe Fornecedor e o método calcular salário

---
+++ src/br/com/zup/ModelagemDeClasses/Colaborador.java
@@ -0,0 +1,52 @@
+package br.com.zup.ModelagemDeClasses;
+
+/*
+Considere, como outra subclasse da classe Pessoa, a classe Colaborador. Considere que cada instância da classe
+Colaborador tem, para além dos atributos que caracterizam a classe Pessoa, os atributos codigoSetor (inteiro),
+salarioBase (double) e imposto (double).
+ */
+public class Colaborador extends Pessoa{
+    //Atributos
+    private int codigoSetor;
+    private double salarioBase;
+    private double imposto;
+
+    //Método construtor
+    public Colaborador(String nome, String endereco, String telefone, int codigoSetor, double salarioBase, double imposto) {
+        super(nome, endereco, telefone);
+        this.codigoSetor = codigoSetor;
+        this.salarioBase = salarioBase;
+        this.imposto = imposto;
+    }
+
+    //Getters and Setters
+
+    public int getCodigoSetor() {
+        return codigoSetor;
+    }
+
+    public void setCodigoSetor(int codigoSetor) {
+        this.codigoSetor = codigoSetor;
+    }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setSalarioBase(double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+
+    public double getImposto() {
+        return imposto;
+    }
+
+    public void setImposto(double imposto) {
+        this.imposto = imposto;
+    }
+
+    //Método calcular salario
+    public double calcularSalario(){
+        return salarioBase - imposto;
+    }
+}
