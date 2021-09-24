dbe90f669d7715d2b565aca5db2d62582959e96c
Let Marçal <leticiamarcalmatias@gmail.com>
2021-09-23 21:52:57 -0300

    Classe Administrador completa

--- src/br/com/zup/ModelagemDeClasses/Administrador.java
+++ src/br/com/zup/ModelagemDeClasses/Administrador.java
@@ -1,2 +1,29 @@
-package br.com.zup.ModelagemDeClasses;public class Administrador {
+package br.com.zup.ModelagemDeClasses;
+
+public class Administrador extends Colaborador{
+    //Atributos
+    private double ajudaDeCusto;
+
+    //Método construtor
+    public Administrador(String nome, String endereco, String telefone, int codigoSetor, double salarioBase, double imposto, double ajudaDeCusto) {
+        super(nome, endereco, telefone, codigoSetor, salarioBase, imposto);
+        this.ajudaDeCusto = ajudaDeCusto;
+    }
+
+    //Getters and Setters
+    public double getAjudaDeCusto() {
+        return ajudaDeCusto;
+    }
+
+    public void setAjudaDeCusto(double ajudaDeCusto) {
+        this.ajudaDeCusto = ajudaDeCusto;
+    }
+
+    @Override
+    public  double calcularSalario(){
+        double salarioLiquido = ajudaDeCusto + (getSalarioBase() - getImposto());
+        return salarioLiquido;
+    }
+
+
 }
